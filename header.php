7e4bbd092481a848a105b56ed03d15f13d76922a
MyoMinHtwe <45064883+MyoMinHtwe@users.noreply.github.com>
2020-01-13 09:52:03 +0800

    Commit header

---
+++ header.php
@@ -0,0 +1,10 @@
+< !DOCTYPE html>
+<html>
+<head>
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+</head>
+<body>
+  <h2>This is header. </h2>
