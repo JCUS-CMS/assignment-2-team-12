c8a661362704a5de141efff22c0bbcfe33c1a697
Myo Min Htwe <myomin.htwe@my.jcu.edu.au>
2020-01-13 11:49:16 +0800

    footer commit

--- themes/twentytwenty/footer.php
+++ themes/twentytwenty/footer.php
@@ -30,7 +30,7 @@
 
 						<p class="powered-by-wordpress">
 							<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentytwenty' ) ); ?>">
-								<?php _e( 'Powered by WordPress', 'twentytwenty' ); ?>
+								<?php _e( 'Powered by Myo Min Htwe', 'twentytwenty' ); ?>
 							</a>
 						</p><!-- .powered-by-wordpress -->
 
