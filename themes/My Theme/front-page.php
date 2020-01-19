9da8a3132a707871880b94fb86433ce34cf38841
Sai Aung Sein Lin <saiaungseinlin@my.jcu.edu.au>
2020-01-19 14:59:38 +0800

    commit

--- themes/My Theme/front-page.php
+++ themes/My Theme/front-page.php
@@ -1,20 +1,11 @@
-<?php get_header('secondary');?>
+<?php get_header();?>
 
 <div class="container">
 
         <h1><?php the_title();?></h1>
 
-<<<<<<< Updated upstream
-        <h1><?php the_title();?></h1>
-
         <?php get_template_part('includes/section','content');?>
 
 </div>
 
-=======
-        <?php get_template_part('includes/section','content');?>
-
-</div>
->>>>>>> Stashed changes
-
-<?php get_footer();?>
+<?php get_footer();?>  
--- themes/My Theme/includes/section-content.php
+++ themes/My Theme/includes/section-content.php
@@ -1,13 +1,5 @@
-<<<<<<< Updated upstream
-<?php if( have_posts() ): while( have_posts() ): the_posts();?>
-
-    <?php the_content();?>
-
-<?php endwhile; else: endif;?>
-=======
 <?php if( have_posts() ): while( have_posts() ): the_post();?>
 
     <?php the_content();?>
 
 <?php endwhile; else: endif;?>  
->>>>>>> Stashed changes
