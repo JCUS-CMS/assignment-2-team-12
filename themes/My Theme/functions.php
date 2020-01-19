e6c6a4a37a19c94f156f47ae81e64bcc80e416c3
Ahnt Moe Heinn <ahntmoeheinn@my.jcu.edu.au>
2020-01-19 17:04:44 +0800

    Top Menu and Mobile Menu Location Initilization

--- themes/My Theme/functions.php
+++ themes/My Theme/functions.php
@@ -36,4 +36,9 @@ add_theme_support('menus');
 #Menus
 register_nav_menus(
 
-)
+	array(
+		'top-menu' => 'Top Menu Location',
+		'mobile-menu' => 'Mobile Menu Location',
+	)
+
+);
--- themes/My Theme/header.php
+++ themes/My Theme/header.php
@@ -14,5 +14,13 @@
 <body>
 	
 <header>
+	<?php
+	wp_nav_menu(
+		array(
+			'top-menu' => 'Top Menu Location',
+		'mobile-menu' => 'Mobile Menu Location',
+		)
+	);
+	?>
 	
 </header>
