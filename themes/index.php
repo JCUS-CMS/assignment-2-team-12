da8e4427369179edd292cf01ce64834ae2649a99
Myo Min Htwe <myomin.htwe@my.jcu.edu.au>
2020-01-19 23:25:40 +0800

    index changed

--- themes/index.php
+++ themes/index.php
@@ -12,3 +12,17 @@
  *
  * @package Airi
  */
+ get_header(); ?>
+
+ 	<div id="primary" class="content-area">
+ 		<main id="main" class="site-main">
+
+ 		<?php
+ 		if ( have_posts() ) :
+
+ 			if ( is_home() && ! is_front_page() ) : ?>
+ 				<header>
+ 					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+ 				</header>
+
+ 			<?php
