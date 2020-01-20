325709544c6615fbec03eb13346dae72aec5deb1
EdyZ01 <aungzayya1@my.jcu.edu.au>
2020-01-20 15:14:18 +0800

    Test push

--- themes/My Theme/includes/section-blogcontent.php
+++ themes/My Theme/includes/section-blogcontent.php
@@ -10,10 +10,11 @@
 	?>
 
 	<p>Posted By : <? echo $fname;?> <?php echo $lname;?></p>
-
+	this is the footter
 
 	<?php
   	$tags = get_the_tags();
+  	if($tags) :
 	foreach($tags as $tag):?>
 
 			 <a href="<?php echo get_tag_link($tag->term_id);?>"> class = "badge badge-success">
@@ -22,11 +23,19 @@
 
 	<?php endforeach; ?>
 
+    <?php 
+    $categories = get_the_category();
+    foreach ($categories as $cat) : ?>
+        
+            <a href="<?php echo get_category_link($cat->term_id); ?>">
+                <?php echo $cat->name; ?>
+            </a>
 
-	
-   
+    <?php endforeach; ?>
+
+
     <?php the_content();?>
-
-
-
+    
+    
+    
 <?php endwhile; else: endif;?>  
