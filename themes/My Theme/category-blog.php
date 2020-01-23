08c46e151184c56ac67afb9f29b45bbdeaaf2c0d
EdyZ01 <aungzayya1@my.jcu.edu.au>
2020-01-23 21:56:53 +0800

    blog testing

---
+++ themes/My Theme/category-blog.php
@@ -0,0 +1,17 @@
+<?php get_header();?>
+
+
+<section class="page-wrap">  
+
+<div class="container">
+
+		<h1><?php echo single_cat_title();?></h1>
+
+        <?php get_template_part('includes/section','archive');?>
+
+        <?php previous_posts_link();?>
+        <?php next_posts_link();?> 
+</div>
+</section>
+
+<?php get_footer();?>  
---
+++ themes/My Theme/includes/section-blogcontent.php
@@ -0,0 +1,41 @@
+<?php if( have_posts() ): while( have_posts() ): the_post();?>
+
+
+	<p><?php echo get_the_date('l jS F, Y <br>	 h:i:s');?></p>
+
+
+	<?php
+	$fname = get_the_author_meta('first_name');
+	$lname = get_the_author_meta('last_name');
+	?>
+
+	<p>Posted By : <? echo $fname;?> <?php echo $lname;?></p>
+	this is the footter
+
+	<?php
+  	$tags = get_the_tags();
+  	if($tags) :
+	foreach($tags as $tag):?>
+
+			 <a href="<?php echo get_tag_link($tag->term_id);?>"> class = "badge badge-success">
+				<?php echo $tag->name;?>
+			</a> 
+
+	<?php endforeach; ?>
+
+    <?php 
+    $categories = get_the_category();
+    foreach ($categories as $cat) : ?>
+        
+            <a href="<?php echo get_category_link($cat->term_id); ?>">
+                <?php echo $cat->name; ?>
+            </a>
+
+    <?php endforeach; ?>
+
+
+    <?php the_content();?>
+    /** hello **/
+    
+    
+<?php endwhile; else: endif;?>  
